ffe0d725f4815c0122eb0a2c435f689944e97a2c
aaLiweipeng <31944741+aaLiweipeng@users.noreply.github.com>
2019-12-23 03:03:18 +0800

    完成LauncherDelegate，倒计时启动页

--- xiaoyun_ec/src/main/java/com/lwp/xiaoyun/ec/launcher/LauncherDelegate.java
+++ xiaoyun_ec/src/main/java/com/lwp/xiaoyun/ec/launcher/LauncherDelegate.java
@@ -11,6 +11,7 @@ import com.lwp.xiaoyun_core.delegates.XiaoYunDelegate;
 import com.lwp.xiaoyun_core.util.timer.BaseTimerTask;
 import com.lwp.xiaoyun_core.util.timer.ITimerListener;
 
+import java.text.MessageFormat;
 import java.util.Timer;
 
 import butterknife.BindView;
@@ -29,7 +30,12 @@ public class LauncherDelegate extends XiaoYunDelegate implements ITimerListener 
     @BindView(R2.id.tv_launcher_timer)
     AppCompatTextView mTvTimer = null;
 
+    //计时任务 没开始时！ 或者 任务结束时！！！，
+    // 都将之置为 null ！！
+    //要用的时候 调用 initTimer() 进行初始化即可
     private Timer mTimer = null;
+    //倒计时 5s
+    private int mCount = 5;
 
     @OnClick(R2.id.tv_launcher_timer)
     void onClickTimerView() {
@@ -39,6 +45,7 @@ public class LauncherDelegate extends XiaoYunDelegate implements ITimerListener 
     private void initTimer() {
         mTimer = new Timer();
         final BaseTimerTask task = new BaseTimerTask(this);
+        //立即执行 每秒执行一次
         mTimer.schedule(task,0,1000);
     }
 
@@ -54,6 +61,23 @@ public class LauncherDelegate extends XiaoYunDelegate implements ITimerListener 
 
     @Override
     public void onTimer() {
+        getProxyActivity().runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                //每次执行的 任务单元
 
+                if (mTvTimer != null) {
+                    mTvTimer.setText(MessageFormat.format("跳过\n{0}s",mCount));
+                    mCount--;
+                    if (mCount < 0) {
+                        //倒计时 数完了 技术任务就可以取消了
+                        if (mTimer != null) {
+                            mTimer.cancel();
+                            mTimer = null;
+                        }
+                    }
+                }
+            }
+        });
     }
 }
