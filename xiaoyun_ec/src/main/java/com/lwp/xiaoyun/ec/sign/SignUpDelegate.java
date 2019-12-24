f985f30194ba472fd3558c267cdbfa67fa3ac78d
aaLiweipeng <31944741+aaLiweipeng@users.noreply.github.com>
2019-12-25 01:48:43 +0800

    完成 delegate_sign_up.xml；引入Android Support包、design包依赖

---
+++ xiaoyun_ec/src/main/java/com/lwp/xiaoyun/ec/sign/SignUpDelegate.java
@@ -0,0 +1,28 @@
+package com.lwp.xiaoyun.ec.sign;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+
+import com.lwp.xiaoyun.ec.R;
+import com.lwp.xiaoyun_core.delegates.XiaoYunDelegate;
+
+/**
+ * <pre>
+ *     author : 李蔚蓬（简书_凌川江雪）
+ *     time   : 2019/12/24 20:28
+ *     desc   : 注册逻辑实现
+ * </pre>
+ */
+public class SignUpDelegate extends XiaoYunDelegate {
+
+    @Override
+    public Object setLayout() {
+        return R.layout.delegate_sign_up;
+    }
+
+    @Override
+    public void onBindView(@Nullable Bundle savedInstanceState, View rootView) {
+
+    }
+}
