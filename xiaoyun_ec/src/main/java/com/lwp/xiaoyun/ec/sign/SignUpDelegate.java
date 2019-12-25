7bcbfc24da4bfe5b8b5fb71cad3739e0d6b617a3
aaLiweipeng <31944741+aaLiweipeng@users.noreply.github.com>
2019-12-25 14:51:49 +0800

    完成 SignUpDelegate

--- xiaoyun_ec/src/main/java/com/lwp/xiaoyun/ec/sign/SignUpDelegate.java
+++ xiaoyun_ec/src/main/java/com/lwp/xiaoyun/ec/sign/SignUpDelegate.java
@@ -2,19 +2,106 @@ package com.lwp.xiaoyun.ec.sign;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TextInputEditText;
+import android.util.Patterns;
 import android.view.View;
+import android.widget.Toast;
 
 import com.lwp.xiaoyun.ec.R;
+import com.lwp.xiaoyun.ec.R2;
 import com.lwp.xiaoyun_core.delegates.XiaoYunDelegate;
+import com.lwp.xiaoyun_core.net.RestClient;
+import com.lwp.xiaoyun_core.net.callback.ISuccess;
+
+import butterknife.BindView;
+import butterknife.OnClick;
 
 /**
  * <pre>
  *     author : 李蔚蓬（简书_凌川江雪）
  *     time   : 2019/12/24 20:28
  *     desc   : 注册逻辑实现
+ *              点击注册按钮时，验证注册信息，post提交注册信息
  * </pre>
  */
 public class SignUpDelegate extends XiaoYunDelegate {
+
+    @BindView(R2.id.edit_sign_up_name)
+    TextInputEditText mName = null;
+    @BindView(R2.id.edit_sign_up_email)
+    TextInputEditText mEmail = null;
+    @BindView(R2.id.edit_sign_up_phone)
+    TextInputEditText mPhone = null;
+    @BindView(R2.id.edit_sign_up_password)
+    TextInputEditText mPassword = null;
+    @BindView(R2.id.edit_sign_up_re_password)
+    TextInputEditText mRePassword = null;
+
+    @OnClick(R2.id.btn_sign_up)
+    void onClickSignUp() {
+        //点击 注册按钮
+        if (checkForm()) {
+            //如果 用户输入的注册信息没问题
+
+//            RestClient.builder()
+//                    .url("sign_up")
+//                    .params("","")
+//                    .success(new ISuccess() {
+//                        @Override
+//                        public void onSuccess(String response) {
+//
+//                        }
+//                    })
+//                    .build()
+//                    .post();
+            Toast.makeText(getContext(), "验证通过", Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+    //验证 输入信息 是否符合格式
+    private boolean checkForm() {
+        final String name = mName.getText().toString();
+        final String email = mEmail.getText().toString();
+        final String phone = mPhone.getText().toString();
+        final String password = mPassword.getText().toString();
+        final String rePassword = mRePassword.getText().toString();
+
+        boolean isPass = true;
+
+        //验证 输入信息 是否符合格式
+        if (name.isEmpty()) {
+            mName.setError("请输入姓名~ ");
+            isPass = false;
+        } else {
+            mName.setError(null);
+        }
+        if (email.isEmpty() || !Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            mEmail.setError("错误的邮箱格式 ");
+            isPass = false;
+        } else {
+            mEmail.setError(null);
+        }
+        if (phone.isEmpty() || phone.length() != 11) {
+            mPhone.setError("手机号码错误");
+            isPass = false;
+        } else {
+            mPhone.setError(null);
+        }
+        if (password.isEmpty() || password.length() < 6) {
+            mPassword.setError("请填写至少6位数密码 ");
+            isPass = false;
+        } else {
+            mPassword.setError(null);
+        }
+        if (rePassword.isEmpty() || rePassword.length() < 6 || !(rePassword.equals(password))) {
+            mRePassword.setError("密码验证错误 ");
+            isPass = false;
+        } else {
+            mRePassword.setError(null);
+        }
+        return isPass;
+    }
 
     @Override
     public Object setLayout() {
