047af002453295d38cb1202f68232d34410b80a4
aaLiweipeng <31944741+aaLiweipeng@users.noreply.github.com>
2019-12-30 04:08:16 +0800

    完成注解PayEntryGenerator、AppRegisterGenerator

---
+++ xiaoyun_annotations/src/main/java/com/lwp/xiaoyun_annotations/PayEntryGenerator.java
@@ -0,0 +1,22 @@
+package com.lwp.xiaoyun_annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * <pre>
+ *     author : 李蔚蓬（简书_凌川江雪）
+ *     time   : 2019/12/30 4:04
+ *     desc   :
+ * </pre>
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface PayEntryGenerator {
+
+    String packageName();
+
+    Class<?> payEntryTemplete();
+}
