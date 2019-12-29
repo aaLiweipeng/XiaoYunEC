0989307edfd107be3fe754dedede5d2cbdac746a
aaLiweipeng <31944741+aaLiweipeng@users.noreply.github.com>
2019-12-30 06:34:35 +0800

    完成EntryVisitor

---
+++ xiaoyun_compiler/src/main/java/com/lwp/xiaoyun_compiler/EntryVisitor.java
@@ -0,0 +1,72 @@
+package com.lwp.xiaoyun_compiler;
+
+import com.squareup.javapoet.JavaFile;
+import com.squareup.javapoet.TypeName;
+import com.squareup.javapoet.TypeSpec;
+
+import java.io.IOException;
+
+import javax.annotation.processing.Filer;
+import javax.lang.model.element.Modifier;
+import javax.lang.model.type.TypeMirror;
+import javax.lang.model.util.SimpleAnnotationValueVisitor7;
+
+/**
+ * <pre>
+ *     author : 李蔚蓬（简书_凌川江雪）
+ *     time   : 2019/12/30 5:58
+ *     desc   :
+ * </pre>
+ */
+public final class EntryVisitor extends SimpleAnnotationValueVisitor7<Void, Void> {
+
+    private Filer mFiler = null;//需要被遍历的东西
+    private TypeMirror mTypeMirror = null;//要循环找的类型
+    private String mPackageName = null;//最终要拿到的 包名,在注解里面写入的PackageName
+
+    public void setFiler(Filer filer) {
+        this.mFiler = filer;
+    }
+
+    @Override
+    public Void visitString(String s, Void p) {
+        mPackageName = s;
+        return p;
+    }
+
+    //找出 注解类型 所注解的类 上面所标注的原信息，并且来生成我们的代码
+    @Override
+    public Void visitType(TypeMirror t, Void p) {
+        mTypeMirror = t;
+        generateJavaCode();
+        return p;
+    }
+
+    //TypeSpec 是 javapoet 里面的类，这个类就是帮我们生成我们需要的类，
+    // Type 指的就是 Class，具体可以在 GitHub上 看 javapoet 的使用，
+    // 这里都是标准的使用方式
+    //生成 模板代码
+    private void generateJavaCode() {
+
+        //classBuilder 传入 要生成的类的类名，
+        // 微信接入 要求的类是 WXEntryActivity
+        final TypeSpec targetActivity =
+                TypeSpec.classBuilder("WXEntryActivity")
+                        .addModifiers(Modifier.PUBLIC)
+                        .addModifiers(Modifier.FINAL)
+                        .superclass(TypeName.get(mTypeMirror))//要生成的类要继承的是 从注解中拿出来的 模板类类型
+                        .build();
+
+        // JavaFile.builder( 最终包名 + 类名 )
+        final JavaFile javaFile = JavaFile.builder(mPackageName+".wxapi",targetActivity)
+                .addFileComment("微信入口文件")// 文件注释
+                .build();
+
+        try {
+            javaFile.writeTo(mFiler);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
---
+++ xiaoyun_compiler/src/main/java/com/lwp/xiaoyun_compiler/XiaoYunProcessor.java
@@ -0,0 +1,92 @@
+package com.lwp.xiaoyun_compiler;
+
+import com.google.auto.service.AutoService;
+import com.lwp.xiaoyun_annotations.AppRegisterGenerator;
+import com.lwp.xiaoyun_annotations.EntryGenerator;
+import com.lwp.xiaoyun_annotations.PayEntryGenerator;
+
+import java.lang.annotation.Annotation;
+import java.lang.annotation.ElementType;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.Processor;
+import javax.annotation.processing.RoundEnvironment;
+import javax.lang.model.element.AnnotationMirror;
+import javax.lang.model.element.AnnotationValue;
+import javax.lang.model.element.AnnotationValueVisitor;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.ExecutableElement;
+import javax.lang.model.element.TypeElement;
+
+/**
+ * <pre>
+ *     author : 李蔚蓬（简书_凌川江雪）
+ *     time   : 2019/12/30 4:09
+ *     desc   : 效仿 ButterKnifeProcessor 来完成
+ *              https://github.com/JakeWharton/butterknife/blob/master/butterknife-compiler/src/main/java/butterknife/compiler/ButterKnifeProcessor.java
+ * </pre>
+ */
+
+@SuppressWarnings("unused")
+@AutoService(Processor.class)//使用AutoService 自动生成spi 信息代码
+public class XiaoYunProcessor extends AbstractProcessor {
+
+    //循环取出 getSupportedAnnotations() 中的 Set 的元素
+    //将 <Class<? extends Annotation> 类型的Set 迭代转化成 <String> 类型的 Set
+    @Override
+    public Set<String> getSupportedAnnotationTypes() {
+        final Set<String> types = new LinkedHashSet<>();
+        final Set<Class<? extends Annotation>> supportAnnotations = getSupportedAnnotations();
+        for (Class<? extends Annotation> annotation : supportAnnotations) {
+            types.add(annotation.getCanonicalName());
+        }
+        return types;
+    }
+
+    //用一个 Set 存储所有要用到的 注解类类型
+    private Set<Class<? extends Annotation>> getSupportedAnnotations() {
+
+        final Set<Class<? extends Annotation>> annotations = new LinkedHashSet<>();
+        annotations.add(EntryGenerator.class);
+        annotations.add(PayEntryGenerator.class);
+        annotations.add(AppRegisterGenerator.class);
+        return annotations;
+    }
+
+    @Override
+    public boolean process(Set<? extends TypeElement> set, RoundEnvironment roundEnvironment) {
+
+        return false;
+    }
+
+    private void scan(RoundEnvironment env, Class<? extends Annotation> annotation,
+                      AnnotationValueVisitor visitor) {
+
+        //env 类似相当于 整个代码的环境
+        for (Element typeElement : env.getElementsAnnotatedWith(annotation)) {
+            final List<? extends AnnotationMirror> annotationMirrors =
+                    typeElement.getAnnotationMirrors();
+
+            //嵌套循环
+            for (AnnotationMirror annotationMirror : annotationMirrors) {
+                final Map<? extends ExecutableElement, ? extends AnnotationValue> elementValues
+                        = annotationMirror.getElementValues();
+
+                for (Map.Entry<? extends ExecutableElement, ? extends AnnotationValue> entry
+                        : elementValues.entrySet()) {
+                    entry.getValue().accept(visitor, null);
+                }
+            }
+        }
+    }
+
+    //visitor 相当于 注解 所注解的类、变量、方法 里面 所传入的值
+
+    private void generateEntryCode(RoundEnvironment env) {
+
+    }
+}
