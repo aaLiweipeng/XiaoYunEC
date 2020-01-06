9471e2bafb1437924706fda208bf9056fad61d3b
aaLiweipeng <31944741+aaLiweipeng@users.noreply.github.com>
2020-01-06 19:49:36 +0800

    IndexDelegate中，初始化组件

--- xiaoyun_ec/src/main/java/com/lwp/xiaoyun/ec/main/index/IndexDelegate.java
+++ xiaoyun_ec/src/main/java/com/lwp/xiaoyun/ec/main/index/IndexDelegate.java
@@ -2,10 +2,18 @@ package com.lwp.xiaoyun.ec.main.index;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.AppCompatEditText;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import com.joanzapata.iconify.widget.IconTextView;
 import com.lwp.xiaoyun.ec.R;
+import com.lwp.xiaoyun.ec.R2;
 import com.lwp.xiaoyun_core.delegates.bottom.BottomItemDelegate;
+
+import butterknife.BindView;
 
 /**
  * <pre>
@@ -15,6 +23,18 @@ import com.lwp.xiaoyun_core.delegates.bottom.BottomItemDelegate;
  * </pre>
  */
 public class IndexDelegate extends BottomItemDelegate {
+
+    @BindView(R2.id.rv_index)
+    RecyclerView mRecyclerView = null;
+    @BindView(R2.id.srl_index)
+    SwipeRefreshLayout mRefreshLayout = null;
+    @BindView(R2.id.tb_index)
+    Toolbar mToolbar = null;
+    @BindView(R2.id.icon_index_scan)
+    IconTextView mIconScan = null;
+    @BindView(R2.id.et_search_view)
+    AppCompatEditText mSearchView = null;
+
     @Override
     public Object setLayout() {
         return R.layout.delegate_index;
