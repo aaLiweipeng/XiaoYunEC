d98c8f31f37a28d4e5d66a68921d491555cc9dc5
aaLiweipeng <31944741+aaLiweipeng@users.noreply.github.com>
2020-01-16 03:24:33 +0800

    xiaoyun_core\ui\refresh\ 下，新建PagingBean.java，编写完成；

---
+++ xiaoyun_core/src/main/java/com/lwp/xiaoyun_core/ui/refresh/PagingBean.java
@@ -0,0 +1,76 @@
+package com.lwp.xiaoyun_core.ui.refresh;
+
+/**
+ * <pre>
+ *     author : 李蔚蓬（简书_凌川江雪）
+ *     time   : 2020/1/15 22:45
+ *     desc   : 创建五个数据分页逻辑相关 需要的常量
+ *              编写对应的getter setter
+ *              setter改成链式调用的模式
+ *
+ * </pre>
+ */
+public final class PagingBean {
+
+    //当前是第几页
+    private int mPageIndex = 0;
+    //总数据条数
+    private int mTotal = 0;
+    //一页显示几条数据
+    private int mPageSize = 0;
+    //当前已经显示了几条数据
+    private int mCurrentCount = 0;
+    //加载延迟
+    private int mDelayed = 0;
+
+    public int getPageIndex() {
+        return mPageIndex;
+    }
+
+    public PagingBean setPageIndex(int mPageIndex) {
+        this.mPageIndex = mPageIndex;
+        return this;
+    }
+
+    public int getTotal() {
+        return mTotal;
+    }
+
+    public PagingBean setTotal(int mTotal) {
+        this.mTotal = mTotal;
+        return this;
+    }
+
+    public int getPageSize() {
+        return mPageSize;
+    }
+
+    public PagingBean setPageSize(int mPageSize) {
+        this.mPageSize = mPageSize;
+        return this;
+    }
+
+    public int getCurrentCount() {
+        return mCurrentCount;
+    }
+
+    public PagingBean setCurrentCount(int mCurrentCount) {
+        this.mCurrentCount = mCurrentCount;
+        return this;
+    }
+
+    public int getDelayed() {
+        return mDelayed;
+    }
+
+    public PagingBean setDelayed(int mDelayed) {
+        this.mDelayed = mDelayed;
+        return this;
+    }
+
+    //第一次加载第一页的时候 调用
+    PagingBean addIndex() {
+        mPageIndex ++;
+        return this;
+    }
+}
