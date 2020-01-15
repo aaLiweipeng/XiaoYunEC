fae2c2fae2b4773d5b5c7003fcfb3dbf0f971a30
aaLiweipeng <31944741+aaLiweipeng@users.noreply.github.com>
2020-01-16 03:52:31 +0800

    更新RefreshHandler，把RecyclerView、Adapter、DataConverter、PagingBean等因素加入构造器；

--- xiaoyun_core/src/main/java/com/lwp/xiaoyun_core/ui/refresh/RefreshHandler.java
+++ xiaoyun_core/src/main/java/com/lwp/xiaoyun_core/ui/refresh/RefreshHandler.java
@@ -2,12 +2,15 @@ package com.lwp.xiaoyun_core.ui.refresh;
 
 import android.os.Looper;
 import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.RecyclerView;
 import android.widget.Toast;
 
 import com.lwp.xiaoyun_core.app.XiaoYun;
 import com.lwp.xiaoyun_core.net.OkHttpUtil;
 import com.lwp.xiaoyun_core.net.RestClient;
 import com.lwp.xiaoyun_core.net.callback.ISuccess;
+import com.lwp.xiaoyun_core.ui.recycler.DataConverter;
+import com.lwp.xiaoyun_core.ui.recycler.MultipleRecyclerAdapter;
 import com.lwp.xiaoyun_core.util.log.XiaoYunLogger;
 
 import java.io.IOException;
@@ -28,20 +31,38 @@ import okhttp3.Response;
  *                  mRefreshHandler = new RefreshHandler(mRefreshLayout);
  *              }
  *
+ *              RecyclerView、Adapter、DataConverter、PagingBean
  * </pre>
  */
 public class RefreshHandler implements SwipeRefreshLayout.OnRefreshListener {
 
     private final SwipeRefreshLayout REFRESH_LAYOUT;
+    private final PagingBean BEAN;
+    private final RecyclerView RECYCLERVIEW;
+    private MultipleRecyclerAdapter mAdapter = null;
+    private final DataConverter CONVERTER;
+
 
     private static final String TAG = "RefreshHandler";
 
     // 构造方法 接收外部传进来的 SwipeRefreshLayout实例
-    // 以及 为之 设置 刷新监听器——即本类，（注意回顾 SwipeRefreshLayout的 刷新监听方法）
+    // 以及 为之 设置 刷新监听器(OnRefreshListener)——即本类，
+    // （注意回顾 SwipeRefreshLayout的 刷新监听方法）,监听回调方法为 onRefresh()
     // 本类继承自 OnRefreshListener，可以封装实现更多的刷新逻辑，放置到 监听回调方法 中
-    public RefreshHandler(SwipeRefreshLayout swipeRefreshLayout) {
+    RefreshHandler(SwipeRefreshLayout swipeRefreshLayout,
+                          RecyclerView recyclerView,DataConverter converter,
+                          PagingBean bean) {
         REFRESH_LAYOUT = swipeRefreshLayout;
+        RECYCLERVIEW = recyclerView;
+        CONVERTER = converter;
+        BEAN = bean;
         REFRESH_LAYOUT.setOnRefreshListener(this);
+    }
+
+    //简单工厂方法（静态的create()）
+    public static RefreshHandler create(SwipeRefreshLayout swipeRefreshLayout,
+                                        RecyclerView recyclerView,DataConverter converter) {
+        return new RefreshHandler(swipeRefreshLayout, recyclerView, converter, new PagingBean());
     }
 
     //测试 封装固有的 刷新前准备套路
