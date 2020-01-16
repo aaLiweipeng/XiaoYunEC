03a066806e6649e7d7b1df7f1385419e78ca92c6
aaLiweipeng <31944741+aaLiweipeng@users.noreply.github.com>
2020-01-17 01:34:56 +0800

    把example中的colors.xml复制到core\src\main\res\values\ 以及ec\src\main\res\values\  下，在ec中重新编写；在IndexDelegate中使用；运行测试成功：

--- xiaoyun_ec/src/main/java/com/lwp/xiaoyun/ec/main/index/IndexDelegate.java
+++ xiaoyun_ec/src/main/java/com/lwp/xiaoyun/ec/main/index/IndexDelegate.java
@@ -3,6 +3,7 @@ package com.lwp.xiaoyun.ec.main.index;
 import android.os.Bundle;
 import android.os.Looper;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.AppCompatEditText;
 import android.support.v7.widget.GridLayoutManager;
@@ -16,6 +17,7 @@ import com.lwp.xiaoyun.ec.R;
 import com.lwp.xiaoyun.ec.R2;
 import com.lwp.xiaoyun_core.delegates.bottom.BottomItemDelegate;
 import com.lwp.xiaoyun_core.net.OkHttpUtil;
+import com.lwp.xiaoyun_core.ui.recycler.BaseDecoration;
 import com.lwp.xiaoyun_core.ui.recycler.MultipleFields;
 import com.lwp.xiaoyun_core.ui.recycler.MultipleItemEntity;
 import com.lwp.xiaoyun_core.ui.refresh.RefreshHandler;
@@ -97,7 +99,8 @@ public class IndexDelegate extends BottomItemDelegate {
     private void initRecyclerView() {
         final GridLayoutManager manager = new GridLayoutManager(getContext(), 4);
         mRecyclerView.setLayoutManager(manager);
-
+        mRecyclerView.addItemDecoration(
+                BaseDecoration.create(ContextCompat.getColor(getContext(), R.color.app_background),5));
     }
 
     @Override
