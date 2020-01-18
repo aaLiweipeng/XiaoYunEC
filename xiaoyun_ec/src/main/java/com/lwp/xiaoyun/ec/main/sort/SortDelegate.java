232c6ccd4d4217a893c89a959c6c510f481e5a60
aaLiweipeng <31944741+aaLiweipeng@users.noreply.github.com>
2020-01-19 00:17:34 +0800

    Sort/list 下，新建VerticalListDelegate；创建并完成delegate_vertical_list.xml；

--- xiaoyun_ec/src/main/java/com/lwp/xiaoyun/ec/main/sort/SortDelegate.java
+++ xiaoyun_ec/src/main/java/com/lwp/xiaoyun/ec/main/sort/SortDelegate.java
@@ -25,4 +25,13 @@ public class SortDelegate extends BottomItemDelegate {
     public void onBindView(@Nullable Bundle savedInstanceState, View rootView) {
 
     }
+
+    //把渲染逻辑写在这里，这样的话，在打开其他平级的时候，
+    // 打开到别的tab，如主页的时候，分类是不会加载的，
+    // 只有点到分类tab按钮，FrameLayout容器切到本页，本页才会开始渲染布局！！
+    // 如果直接写在 onBindView中，打开其他tab对应的页时，本页已经开始加载了，这是不好的
+    @Override
+    public void onLazyInitView(@Nullable Bundle savedInstanceState) {
+        super.onLazyInitView(savedInstanceState);
+    }
 }
---
+++ xiaoyun_ec/src/main/java/com/lwp/xiaoyun/ec/main/sort/list/VerticalListDelegate.java
@@ -0,0 +1,30 @@
+package com.lwp.xiaoyun.ec.main.sort.list;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+
+import com.lwp.xiaoyun.ec.R;
+import com.lwp.xiaoyun_core.delegates.XiaoYunDelegate;
+
+/**
+ * <pre>
+ *     author : 李蔚蓬（简书_凌川江雪）
+ *     time   : 2020/1/19 0:11
+ *     desc   : 垂直列表Fragment
+ * </pre>
+ */
+public class VerticalListDelegate extends XiaoYunDelegate {
+
+
+
+    @Override
+    public Object setLayout() {
+        return R.layout.delegate_vertical_list;
+    }
+
+    @Override
+    public void onBindView(@Nullable Bundle savedInstanceState, View rootView) {
+
+    }
+}
