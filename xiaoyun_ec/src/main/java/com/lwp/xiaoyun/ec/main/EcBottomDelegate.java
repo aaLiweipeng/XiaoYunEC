325bf69fd040c8546437465a9019b9b3e4efd0ac
aaLiweipeng <31944741+aaLiweipeng@users.noreply.github.com>
2020-01-28 08:26:58 +0800

    ec.main包下 新建一个discover包，包下新建一个DiscoverDelegate，为之创建一个布局delegate_discover.xml并完成；配置DiscoverDelegate的布局，更新EcBottomDelegate，运行测试成功；

--- xiaoyun_ec/src/main/java/com/lwp/xiaoyun/ec/main/EcBottomDelegate.java
+++ xiaoyun_ec/src/main/java/com/lwp/xiaoyun/ec/main/EcBottomDelegate.java
@@ -2,6 +2,7 @@ package com.lwp.xiaoyun.ec.main;
 
 import android.graphics.Color;
 
+import com.lwp.xiaoyun.ec.main.discover.DiscoverDelegate;
 import com.lwp.xiaoyun.ec.main.index.IndexDelegate;
 import com.lwp.xiaoyun.ec.main.sort.SortDelegate;
 import com.lwp.xiaoyun_core.delegates.bottom.BaseBottomDelegate;
@@ -27,7 +28,7 @@ public class EcBottomDelegate extends BaseBottomDelegate {
         final LinkedHashMap<BottomTabBean, BottomItemDelegate> items = new LinkedHashMap<>();
         items.put(new BottomTabBean("{fa-home}", "主页"), new IndexDelegate());
         items.put(new BottomTabBean("{fa-sort}", "分类"), new SortDelegate());
-        items.put(new BottomTabBean("{fa-compass}", "发现"), new IndexDelegate());
+        items.put(new BottomTabBean("{fa-compass}", "发现"), new DiscoverDelegate());
         items.put(new BottomTabBean("{fa-shopping-cart}", "购物车"), new IndexDelegate());
         items.put(new BottomTabBean("{fa-user}", "我的"), new IndexDelegate());
         return builder.addItems(items).build();
---
+++ xiaoyun_ec/src/main/java/com/lwp/xiaoyun/ec/main/discover/DiscoverDelegate.java
@@ -0,0 +1,28 @@
+package com.lwp.xiaoyun.ec.main.discover;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+
+import com.lwp.xiaoyun.ec.R;
+import com.lwp.xiaoyun_core.delegates.bottom.BottomItemDelegate;
+
+/**
+ * <pre>
+ *     author : 李蔚蓬（简书_凌川江雪）
+ *     time   : 2020/1/27 0:47
+ *     desc   :
+ * </pre>
+ */
+public class DiscoverDelegate extends BottomItemDelegate {
+
+    @Override
+    public Object setLayout() {
+        return R.layout.delegate_discover;
+    }
+
+    @Override
+    public void onBindView(@Nullable Bundle savedInstanceState, View rootView) {
+
+    }
+}
